c785a4d4498c2634c8858e7d0374ca868a82e956
Dhaval Solanki <dhavalas24@gmail.com>
2024-12-14 17:08:44 +0530

    changes query..

--- SQL/GroupBy.sql
+++ SQL/GroupBy.sql
@@ -14,7 +14,10 @@ a. Print the description and total qty sold for each product.
 ==>
 
 
-select description,order_status,qty_on_hand "qty_on_hand" from product_master,sales_order
+select pm.description,so.order_status,pm.qty_on_hand "qty_on_hand" from product_master pm
+inner join sales_order_details sod on sod.product_no = pm.product_no
+inner join sales_order so on sod.order_no = so.order_no
+inner join client_master cm on cm.client_no= so.client_no
 where order_status = "fulfilled" 
  Group by order_status
  having sum(qty_on_hand);
@@ -29,5 +32,11 @@ error..............
 
 
 b. Find the value of each product sold.
+
+==>
+
+
+
+
 c. Calculate the average qty sold for each client that has a maximum order value of 15000.00.
 d. Find out the total of all the billed orders for the month of June.
